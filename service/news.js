4c97e0849f8b3ba32e5789be2792e9b6d82c2080
MAPARAB <MAPARAB@users.noreply.github.com>
2017-04-13 17:48:47 +0530

    Made the source dynamix

--- service/news.js
+++ service/news.js
@@ -4,32 +4,37 @@
 var http = require('http');
 
 exports.newsInfo = function(req, res)
-			{
+			{			
+				var apiKey = '&apiKey=078ac7c010ae439ba1ddc3c30c8ddce0';
+				var urlPath = '/v1/articles?source=';	
+ 	 			urlPath = urlPath + req.query.source + apiKey;	
+ 	 			console.log(urlPath);
+	
 				var options = {
 						host: 'newsapi.org',
-						path: '/v1/articles?source=cnn&apiKey=078ac7c010ae439ba1ddc3c30c8ddce0',
+						path: urlPath,
 						method: 'GET'
 						};
 					
 				var request = http.request(options, function(response)
-											{
-												var body = "";
-												
-												response.on('data', function(data) 
-															{
-																body += data;
-															});
-															
-												response.on('end', function()
-															{
-																res.send(JSON.parse(body));
-															});
-											});
+						{
+							var body = "";
+
+							response.on('data', function(data) 
+							{
+								body += data;
+							});
+
+							response.on('end', function()
+							{
+								res.send(JSON.parse(body));
+							});
+						});
 			
 				request.on('error', function(e) 
-							{
-								console.log('Problem with request: ' + e.message);
-							});
+				{
+					console.log('Problem with request: ' + e.message);
+				});
 
 				request.end();				
 				
